6d3529e34d7e46d731c7916def3266e46512b513
Gitesh Khurani <giteshkhurani@Giteshs-MacBook-Pro.local>
2022-09-16 22:50:41 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { EditorView, basicSetup } from "codemirror";
+import { sql } from "@codemirror/lang-sql";
+
+import TheHeaderVue from "./components/TheHeader.vue";
+const content = ref("SELECT * FROM USERS;");
+
+let editor = new EditorView({
+  extensions: [basicSetup, sql()],
+});
+</script>
+
+<template>
+  <div class="h-full">
+    <TheHeaderVue />
+    <main class="max-w-[80%] mx-auto relative h-[90%]">
+      <textarea v-model="content" id="editor"></textarea>
+    </main>
+  </div>
+</template>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="flex px-6 py-4 items-baseline gap-x-4 h-[10%]">
+    <h2>SQL Query Runner</h2>
+    <ul class="flex items-center">
+      <li class="flex items-center">
+        Run
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-5 h-5 text-green-500 fill-current"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+          />
+        </svg>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+li {
+  @apply cursor-pointer;
+}
+</style>
